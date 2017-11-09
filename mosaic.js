8e8464d720b1911564e46752e1df9c636468a00e
Morgen Gramlich <danila.sergeyev@gmail.com>
2017-11-10 01:22:13 +0300

    Calculate cell width & height

--- mosaic.js
+++ mosaic.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function mosaic(colNum, rowNum, imagesArray) {
-    let pan = document.getElementById('photo-mosaic');
-    let images = imagesArray;
+    let pan = document.getElementById('photo-mosaic'),
+        images = imagesArray;
+
+    let cellHeight = pan.offsetHeight / rowNum,
+        cellWidth = pan.offsetWidth / colNum;
+
     for (let i = 0; i < colNum; i++) {
         for (let j = 0; j < rowNum; j++) {
             pan.innerHTML = pan.innerHTML + template(i, j);
@@ -11,20 +15,18 @@ function mosaic(colNum, rowNum, imagesArray) {
 
     setInterval(() => {
         let col, row;
-        do {
-            col = Math.floor(Math.random() * colNum);
-            row = Math.floor(Math.random() * rowNum);
-        } while (col < colNum - 2 - row);
-        document.getElementById('m-el-' + col + '-'+ row).src = randomPhoto();
+        col = Math.floor(Math.random() * colNum);
+        row = Math.floor(Math.random() * rowNum);
+        document.getElementById('m-el-' + col + '-'+ row).style.backgroundImage = 'url(' + randomPhoto() + ')';
     }, 2000);
 
     function template(column, row) {
-        return '<img src="'
-            + randomPhoto()
-            + '" style="grid-column-start: '+ (column+1)
+        return '<div style="grid-column-start: '+ (column+1)
             + '; grid-row-start: '+ (row+1)
-            + '; height: 96px; width: 128px;" id="m-el-'
-            + column + '-'+ row
+            + '; height: ' + cellHeight
+            + 'px; width: ' + cellWidth
+            + 'px; background-image: url(' + randomPhoto() + '); background-size: cover'
+            +'" id="m-el-' + column + '-'+ row
             + '"/>';
     }
 
